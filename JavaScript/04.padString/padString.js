052daa9d2aa0144ce7c506ac5739a4124adefd3a
Bo.Jin <bo.jin@borgor.cn>
2020-06-03 17:53:16 +0800

    fixed padding string by hand.

--- JavaScript/04.padString/padString.js
+++ JavaScript/04.padString/padString.js
@@ -4,7 +4,7 @@
  * Author: Bo.Jin
  * -----
  * Last Modified: Bo.Jin
- * Modified By: 2020-06-03 17:38:48 pm
+ * Modified By: 2020-06-03 17:52:51 pm
  * -----
  * Copyright (c) 2020 ZDWW
  * ------------------------------------
@@ -15,18 +15,34 @@
  */
 
 String.prototype.myPadStart = function (len, slot) {
-  if (len <= this.length) {
-    return this + ''
+  len = len >> 0
+  slot = String(typeof slot !== 'undefined' ? slot : '')
+  if (len <= this.length || slot.length === 0) {
+    return String(this)
   }
-  slot = (slot || ' ') + ''
-  let repeatTimes = Math.ceil((len - this.length) / slot.length)
-  return slot.repeat(repeatTimes).substr(0, len - this.length) + this
+  return String(
+    slot
+      .repeat(Math.ceil((len - this.length) / slot.length))
+      .slice(0, len - this.length) + this
+  )
 }
 String.prototype.myPadEnd = function (len, slot) {
-  if (len <= this.length) {
-    return this + ''
+  len = len >> 0
+  slot = String(typeof slot !== 'undefined' ? slot : '')
+  if (len <= this.length || slot.length === 0) {
+    return String(this)
   }
-  slot = (slot || ' ') + ''
-  let repeatTimes = Math.ceil((len - this.length) / slot.length)
-  return this + slot.repeat(repeatTimes).substr(0, len - this.length)
+  return String(
+    this +
+      slot
+        .repeat(Math.ceil((len - this.length) / slot.length))
+        /**
+         * 此处使用slice的第二个参数其实并不是长度的意思,
+         * 但是因为其开始位置的索引为0, 所以,结束位置的索引自然就是长度了.
+         * 上面的Start函数一样.
+         * 此处不建议使用substr, substr并不是一个JavaScript标准函数,如果非要使用,请使用String.prototype.substring
+         * 参考链接: https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/String/substr
+         */
+        .slice(0, len - this.length)
+  )
 }
--- JavaScript/04.padString/test.js
+++ JavaScript/04.padString/test.js
@@ -4,7 +4,7 @@
  * Author: Bo.Jin
  * -----
  * Last Modified: Bo.Jin
- * Modified By: 2020-06-03 17:39:15 pm
+ * Modified By: 2020-06-03 17:45:40 pm
  * -----
  * Copyright (c) 2020 ZDWW
  * ------------------------------------
@@ -17,3 +17,4 @@
 require('./padString')
 console.log('abcdef'.myPadStart(20, '01'))
 console.log('abcdef'.myPadEnd(20, '012'))
+console.log('abcdef'.myPadEnd(20, ''))
